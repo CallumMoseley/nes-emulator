6e81aec2aa398a03745fc57bb5a4046357d928ea
Callum Moseley <callum_moseley@hotmail.com>
2014-10-07 07:34:36 -0400

    Implemented opcodes up to ROR.

--- NES_Emu/src/CPU.java
+++ NES_Emu/src/CPU.java
@@ -34,11 +34,19 @@ public class CPU
 	
 	public void tick()
 	{
-		for (int i = 0; i < 3; i++)
+		tick(1);
+	}
+	
+	public void tick(int n)
+	{
+		for (int i = 0; i < n; i++)
 		{
-			ppu.tick();
+			for (int j = 0; j < 3; j++)
+			{
+				ppu.tick();
+			}
+			apu.tick();
 		}
-		apu.tick();
 	}
 
 	public void op()
@@ -161,7 +169,6 @@ public class CPU
 		else if (opcode == 0x0A || opcode == 0x06 || opcode == 0x16 || opcode == 0x0E || opcode == 0x1E)
 		{
 			char operand = 0;
-			char zpAddr = 0;
 			char loc = 0;
 			boolean go = true;
 			switch (opcode)
@@ -178,8 +185,7 @@ public class CPU
 			case 0x16:
 				loc = (char) ((accessMemory(++pc) + x) % 256);
 				operand = accessMemory(loc);
-				tick();
-				tick();
+				tick(2);
 			break;
 			case 0x0E:
 				loc = (char) (accessMemory(++pc) | (accessMemory(++pc) << 8));
@@ -189,19 +195,66 @@ public class CPU
 			case 0x1E:
 				loc = (char) ((accessMemory(++pc) | (accessMemory(++pc) << 8)) + x);
 				operand = accessMemory(loc);
-				tick();
-				tick();
+				tick(2);
 			break;
 			}
 			if (go) ASL(operand, loc);
 		}
 		else if (opcode == 0x90)
 		{
-			//BCC
+			BCC(accessMemory(++pc));
 		}
+		else if (opcode == 0xB0)
+		{
+			BCS(accessMemory(++pc));
+		}
+		else if (opcode == 0xF0)
+		{
+			BEQ(accessMemory(++pc));
+		}
+		else if (opcode == 0x24 || opcode == 0x2C)
+		{
+			char operand = 0;
+			switch (opcode)
+			{
+			case 0x24:
+				operand = accessMemory(accessMemory(++pc));
+			break;
+			case 0x2C:
+				operand = (char)(accessMemory(++pc) | (accessMemory(++pc) << 8));
+				tick();
+			break;
+			}
+			BIT(operand);
+		}
+		else if (opcode == 0x30)
+		{
+			BMI(accessMemory(++pc));
+		}
+		else if (opcode == 0xD0)
+		{
+			BNE(accessMemory(++pc));
+		}
+		else if (opcode == 0x10)
+		{
+			BPL(accessMemory(++pc));
+		}
+		else if (opcode == 0x00)
+		{
+			BRK();
+		}
+		else if (opcode == 0x50)
+		{
+			BVC(accessMemory(++pc));
+		}
+		else if (opcode == 0x70)
+		{
+			BVS(accessMemory(++pc));
+		}
+		
 		pc++;
 	}
-	
+
 	public char accessMemory(boolean write, char address, char b)
 	{
 		if (reset && write) write = false;
@@ -255,6 +308,405 @@ public class CPU
 	{
 		c = (byte) (operand >> 7);
 		operand = (char) ((operand << 1) & 0xFF);
+		accessMemory(true, loc, operand);
+		z = 0;
+		if (operand == 0) z = 1;
+		n = (byte) (operand >> 7);
+	}
+	
+	public void BCC(char operand)
+	{
+		if (c == 0)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BCS(char operand)
+	{
+		if (c == 1)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BEQ(char operand)
+	{
+		if (z == 1)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BIT(char operand)
+	{
+		z = 0;
+		if ((a & operand) == 0) z = 1;
+		v = (byte) ((operand & 0x40) >> 6);
+		n = (byte) (operand >> 7);
+	}
+	
+	public void BMI(char operand)
+	{
+		if (n == 1)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BNE(char operand)
+	{
+		if (z == 0)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BPL(char operand)
+	{
+		if (n == 0)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BRK()
+	{
+		accessMemory((char) (pc + 1));
+		accessMemory(true, (char) (0x0100 + sp--), (char) (pc >> 4));
+		accessMemory(true, (char) (0x0100 + sp--), (char) (pc & 0xFF));
+		char status = (char) ((n << 7) | (v << 6) | (1 << 5) | (1 << 4) | (d << 3) | (i << 2) | (z << 1) | (c));
+		accessMemory(true, (char) (0x0100 + sp--), status);
+		pc = (char) (accessMemory((char) 0xFFFE) | (accessMemory((char) 0xFFFF) << 8));
+	}
+	
+	public void BVC(char operand)
+	{
+		if (v == 0)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void BVS(char operand)
+	{
+		if (v == 1)
+		{
+			tick();
+			pc += operand;
+			if (((pc - operand) & 0x0F00) != (pc & 0x0F00))
+			{
+				tick();
+			}
+			pc -= 2;
+		}
+	}
+	
+	public void CLC()
+	{
+		c = 0;
+	}
+	
+	public void CLD()
+	{
+		d = 0;
+	}
+	
+	public void CLI()
+	{
+		i = 0;
+	}
+	
+	public void CLV()
+	{
+		v = 0;
+	}
+	
+	public void CMP(char operand)
+	{
+		c = 0;
+		if (a >= operand) c = 1;
+		z = 0;
+		if (a == operand) z = 1;
+		n = (byte) (((a - operand) & 0xFF) >> 7);
+	}
+	
+	public void CPX(char operand)
+	{
+		c = 0;
+		if (x >= operand) c = 1;
+		z = 0;
+		if (x == operand) z = 1;
+		n = (byte) (((x - operand) & 0xFF) >> 7);
+	}
+	
+	public void CPY(char operand)
+	{
+		c = 0;
+		if (y >= operand) c = 1;
+		z = 0;
+		if (y == operand) z = 1;
+		n = (byte) (((y - operand) & 0xFF) >> 7);
+	}
+	
+	public void DEC(char loc)
+	{
+		char cur = accessMemory(loc);
+		cur--;
+		cur &= 0xFF;
+		accessMemory(true, loc, cur);
+		z = 0;
+		if (cur == 0) z = 1;
+		n = (byte) (cur >> 7);
+	}
+	
+	public void DEX()
+	{
+		tick();
+		x--;
+		x &= 0xFF;
+		z = 0;
+		if (x == 0) z = 1;
+		n = (byte) (x >> 7);
+	}
+	
+	public void DEY()
+	{
+		tick();
+		y--;
+		y &= 0xFF;
+		z = 0;
+		if (y == 0) z = 1;
+		n = (byte) (y >> 7);
+	}
+	
+	public void EOR(char operand)
+	{
+		a ^= operand;
+		z = 0;
+		if (a == 0) z = 1;
+		n = (byte) (a >> 7);
+	}
+	
+	public void INC(char loc)
+	{
+		char cur = accessMemory(loc);
+		cur++;
+		cur &= 0xFF;
+		accessMemory(true, loc, cur);
+		z = 0;
+		if (cur == 0) z = 1;
+		n = (byte) (cur >> 7);
+	}
+	
+	public void INX()
+	{
+		tick();
+		x++;
+		x &= 0xFF;
+		z = 0;
+		if (x == 0) z = 1;
+		n = (byte) (x >> 7);
+	}
+	
+	public void INY()
+	{
+		tick();
+		y++;
+		y &= 0xFF;
+		z = 0;
+		if (y == 0) z = 1;
+		n = (byte) (y >> 7);
+	}
+	
+	public void JMP(char operand)
+	{
+		pc = operand;
+		pc--;
+	}
+	
+	public void JSR(char operand)
+	{
+		accessMemory(true, (char) (0x0100 + sp--), (char) (pc >> 4));
+		accessMemory(true, (char) (0x0100 + sp--), (char) (pc & 0xFF));
+		tick();
+		pc = operand;
+		pc--;
+	}
+	
+	public void LDA(char operand)
+	{
+		a = operand;
+		z = 0;
+		if (a == 0) z = 1;
+		n = (byte) (a >> 7);
+	}
+	
+	public void LDX(char operand)
+	{
+		x = operand;
+		z = 0;
+		if (x == 0) z = 1;
+		n = (byte) (x >> 7);
+	}
+	
+	public void LDY(char operand)
+	{
+		y = operand;
+		z = 0;
+		if (y == 0) z = 1;
+		n = (byte) (y >> 7);
+	}
+	
+	public void LSR()
+	{
+		c = (byte) (a & 0x01);
+		c >>= 1;
+		z = 0;
+		if (c == 0) z = 1;
+		n = 0;
+	}
+	
+	public void LSR(char operand, char loc)
+	{
+		c = (byte) (operand & 0x01);
+		operand >>= 1;
+		accessMemory(true, loc, operand);
+		z = 0;
+		if (operand == 0) z = 1;
+		n = 0;
+	}
+	
+	public void NOP()
+	{
+		tick();
+	}
+	
+	public void ORA(char operand)
+	{
+		a |= operand;
+		z = 0;
+		if (a == 0) z = 1;
+		n = (byte) (a >> 7);
+	}
+	
+	public void PHA()
+	{
+		accessMemory(true, (char) (0x0100 + sp--), a);
+		tick();
+	}
+	
+	public void PHP()
+	{
+		char status = (char) ((n << 7) | (v << 6) | (1 << 5) | (1 << 4) | (d << 3) | (i << 2) | (z << 1) | (c));
+		accessMemory(true, (char) (0x0100 + sp--), status);
+	}
+	
+	public void PLA()
+	{
+		a = accessMemory((char) (0x100 + ++sp));
+		z = 0;
+		if (a == 0) z = 1;
+		n = (byte) (a >> 7);
+	}
+	
+	public void PLP()
+	{
+		char status = accessMemory((char) (0x100 + ++sp));
+		c = (byte) (status & 0x01);
+		z = (byte) ((status >> 1) & 0x01);
+		i = (byte) ((status >> 2) & 0x01);
+		d = (byte) ((status >> 3) & 0x01);
+		v = (byte) ((status >> 6) & 0x01);
+		n = (byte) ((status >> 7) & 0x01);
+	}
+	
+	public void ROL()
+	{
+		byte oldC = c;
+		c = (byte) (a >> 7);
+		a <<= 1;
+		a |= oldC;
+		a &= 0xFF;
+		z = 0;
+		if (a == 0) z = 1;
+		n = (byte) (a >> 7);
+	}
+	
+	public void ROL(char operand, char loc)
+	{
+		byte oldC = c;
+		c = (byte) (operand >> 7);
+		operand <<= 1;
+		operand |= oldC;
+		operand &= 0xFF;
+		accessMemory(true, loc, operand);
+		z = 0;
+		if (operand == 0) z = 1;
+		n = (byte) (operand >> 7);
+	}
+	
+	public void ROR()
+	{
+		byte oldC = c;
+		c = (byte) (a & 0x01);
+		a >>= 1;
+		a |= (char) (oldC << 7);
+		a &= 0xFF;
+		z = 0;
+		if (a == 0) z = 1;
+		n = (byte) (a >> 7);
+	}
+	
+	public void ROR(char operand, char loc)
+	{
+		byte oldC = c;
+		c = (byte) (operand >> 7);
+		operand <<= 1;
+		operand |= oldC;
+		operand &= 0xFF;
 		accessMemory(true, loc, operand);
 		z = 0;
 		if (operand == 0) z = 1;
